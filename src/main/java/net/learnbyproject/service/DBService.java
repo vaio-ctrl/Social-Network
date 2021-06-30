9014c844d1081f5de8e00345a62d5dc09a6cd93e
vaio-ctrl <85823236+vaio-ctrl@users.noreply.github.com>
2021-06-30 20:22:30 +0530

    Create DBService.java
    The 4 variables DRIVER, URL, USER, and PASSWORD are used to store configurations read from the web.xml.
    
    These variables then will be accessed by the openConnection() method to establish a connection to the database system.

---
+++ src/main/java/net/learnbyproject/service/DBService.java
@@ -0,0 +1,31 @@
+package net.learnbyproject.service;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+public class DBService {
+    public static String DRIVER;
+    public static String URL;
+    public static String USER;
+    public static String PASSWORD;
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+    public static Connection openConnection() {
+        try {
+            Class.forName(DRIVER);
+            Connection c = DriverManager.getConnection(URL, USER, PASSWORD);
+            return c;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }   
+    public static String getFormattedDate(Date date) {
+        return new SimpleDateFormat(DATE_PATTERN).format(date);
+    }
+    public static Date getFormattedDate(String date) throws ParseException{
+        SimpleDateFormat sf = new SimpleDateFormat(DATE_PATTERN);
+        java.util.Date parsedDate = sf.parse(date);
+        return new Date(parsedDate.getTime());
+    }
+}
